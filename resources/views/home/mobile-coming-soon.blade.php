8af5dd9fbbfc1d6ece315b27c8f799a803aff698
dhtntmedia <daniel.harding@tntmedia.co.uk>
2021-10-14 11:32:44 +0100

    Laravel CMS added and mobile version.

---
+++ resources/views/home/mobile-coming-soon.blade.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html data-wf-page="60c1cc1d37fad3862030b52f">
+<head>
+    <meta charset="utf-8">
+    <title>TNT Media - Launch Day</title>
+    <!-- Behavioral Meta Data -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta property="og:title" content="TNT Media - Launch Day" />
+    <meta property="og:url" content="https://www.tntmedia.co.uk" />
+    <meta property="og:image" content="https://www.tntmedia.co.uk/public/assets/images/TNT-media-gallexy.png" />
+    <meta name="keywords"
+          content="tnt, tnt media, #tntmedia1111, 1111, launch,
+          tnt digital, tnt.com, tnt.co.uk, tnt media tech ltd, tnt media tech limited" />
+    <meta content="TNT Media" property="twitter:title" />
+    <meta content="In the Universe everything moves in the vacuums of space, join us and create some great in your space."
+          property="twitter:description" />
+    <meta property="og:type" content="mobile" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="facebook-domain-verification" content="mwfbfo9q0eo2finvzilcdfqs68secg" />
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-209212263-1">
+    </script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-209212263-1');
+    </script>
+
+    <script type="text/javascript">
+        var cpm = {};
+        (function(h,u,b){
+            var d=h.getElementsByTagName("script")[0],e=h.createElement("script");
+            e.async=true;e.src='https://cookiehub.net/c2/f916896c.js';
+            e.onload=function(){u.cookiehub.load(b);}
+            d.parentNode.insertBefore(e,d);
+        })(document,window,cpm);
+    </script>
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <!-- Styles -->
+    <link href="https://shibatoken.com/css/shiba-ui.css" rel="stylesheet" type="text/css" />
+    <link href="images/favicon.png" rel="shortcut icon" type="image/x-icon" />
+    <link href="images/webclip.png" rel="apple-touch-icon" />
+    <link rel="stylesheet" type="text/css" href="assets/css/parallax.css"/>
+    <link rel="stylesheet" type="text/css" href="assets/css/main-style.css"/>
+    <link rel="favicon" type="image/x-icon" href="favicon.ico" />
+    <script src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript">
+
+        $(document).ready(function() {
+            var movementStrength = 210;
+            // var height = movementStrength / $(window).height();
+            var width = movementStrength / $(window).width();
+            // var width = $(window).width();
+
+            $("body").mousemove(function(e){
+                var decreace = ($(window).width() - e.pageX);
+                var pageX = ( decreace / ($(window).width() ) * 100);
+                var newvalueX = width * pageX * -1 - 25;
+                $(".comingsoon-one, .comingsoon-two, .comingsoon-four, .titlebar").css("background-position-x", newvalueX+"%");
+            });
+        });
+    </script>
+
+    <!--Start of HappyFox Live Chat Script-->
+    <script>
+        window.HFCHAT_CONFIG = {
+            EMBED_TOKEN: 'a1b80c70-2b7e-11ec-9f35-a77ac79b9fa6',
+            ASSETS_URL: 'https://widget.happyfoxchat.com/v2/visitor'
+        };
+        (function () {
+            var scriptTag = document.createElement('script')
+            scriptTag.type = 'text/javascript'
+            scriptTag.async = true
+            scriptTag.src = window.HFCHAT_CONFIG.ASSETS_URL + '/js/widget-loader.js'
+
+            var s = document.getElementsByTagName('script')[0]
+            s.parentNode.insertBefore(scriptTag, s)
+        })()
+    </script>
+    <!--End of HappyFox Live Chat Script-->
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '4497869476922952');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=4497869476922952&ev=PageView&noscript=1"
+        /></noscript>
+    <!-- End Facebook Pixel Code -->
+
+</head>
+<body class="body" style="background: url('/assets/images/background-cartoon/stars.jpg') center no-repeat #000;">
+    <div id="how-to-buy" class="section__htb" style="background: none; margin-top: 20px;">
+        <div style="max-width: 500px; margin: auto;">
+            <div class="base-circle-wrap" style="position:relative;">
+                <div class="light orange d phase-3"></div>
+                <img src="/assets/images/mobile/sun.png" loading="lazy" alt="" class="htb-circle-1 light phase-6" />
+                <img src="https://shibatoken.com/images/circle-dashes.svg" loading="lazy" alt="" class="htb-circle-dashes" />
+                <div class="circle-glued-coins">
+                    <div style="max-width: 100px; margin: auto;">
+                        <div class="base-circle-wrap" style="position:relative;">
+                            <img src="/assets/images/mobile/earth.png" style="width:80px;" loading="lazy" alt="" class="htb-circle-1" />
+                            <div class="circle-glued-coins">
+                                <img src="/assets/images/mobile/moon.png" style="width:80px;" loading="lazy" alt="" class="gc-1" />
+                            </div>
+                        </div>
+                    </div>
+                    <img src="https://shibatoken.com/images/gc2.png" style="visibility: hidden;" loading="lazy" alt="" class="gc-2" />
+                    <img src="https://shibatoken.com/images/gc4.png" style="visibility: hidden;" loading="lazy" alt="" class="gc-3" />
+                </div>
+                <div class="front-coins-holder">
+                    <img src="/assets/images/mobile/asteroid.png" loading="lazy" alt="" class="bc-1" />
+                    <img src="/assets/images/mobile/asteroid-2.png" loading="lazy" alt="" class="bc-2" />
+                    <img src="/assets/images/mobile/asteroid-3.png" loading="lazy" alt="" class="bc-3" />
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="container" class="wrapper container" style="margin-top: 0px;">
+        <ul id="scene" class="scene unselectable"
+            data-friction-x="0.1"
+            data-friction-y="0.1"
+            data-scalar-x="25"
+            data-scalar-y="15">
+            <li class="layer" data-depth="0.3">
+                <img id="logo" height="130" src="assets/images/logo-gold.svg"/>
+                <p class="comingsoon-one" style="margin-top: 260px; font-size: 50px !important;">Coming Soon</p>
+                <p class="comingsoon-three" style="margin-top: 10px; color: #fff;">11<span style="color: #bea671 !important;">.</span>11</p>
+                <a href="https://qrco.de/bcRxRD">
+                    <p class="comingsoon-four d-none d-sm-block">Save the date</p>
+                    <img id="qr-code" alt="TNT Media Logo"
+                         height="290" src="assets/images/coming-soon-QR.svg"/>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=60c1cc1d37fad34d6a30b52e" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous" ></script>
+    <script src="https://shibatoken.com/js/shiba-ui.js" type="text/javascript"></script>
+</body>
+</html>
