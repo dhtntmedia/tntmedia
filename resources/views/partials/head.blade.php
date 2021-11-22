042101593470438c58c74bb7e0818b06bd8262d2
dhtntmedia <daniel.harding@tntmedia.co.uk>
2021-11-22 17:24:09 +0000

    Re-added GA and FB script/tags.

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -31,3 +31,38 @@
 @stack('styles')
 <link rel="stylesheet" href="{{ asset('css/header.css') }}?v=4" />
 <link rel="stylesheet" href="{{ asset('css/footer.css') }}?v=4" />
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-209212263-1">
+</script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-209212263-1');
+</script>
+
+<script type="text/javascript">
+    var cpm = {};
+    (function(h,u,b){
+        var d=h.getElementsByTagName("script")[0],e=h.createElement("script");
+        e.async=true;e.src='https://cookiehub.net/c2/f916896c.js';
+        e.onload=function(){u.cookiehub.load(b);}
+        d.parentNode.insertBefore(e,d);
+    })(document,window,cpm);
+</script>
+<!-- Facebook Pixel Code -->
+<script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '4497869476922952');
+    fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=4497869476922952&ev=PageView&noscript=1"
+    /></noscript>
