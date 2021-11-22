d2b797db25f0ee02158ff4eb7ebaead0ee011426
dhtntmedia <daniel.harding@tntmedia.co.uk>
2021-11-22 19:52:00 +0000

    Rotating Facebook image.

--- resources/views/layouts/home/home.blade.php
+++ resources/views/layouts/home/home.blade.php
@@ -26,6 +26,33 @@ fjs.parentNode.insertBefore(js, fjs);
 
 @include('partials.footer')
 
+<!-- Messenger Chat plugin Code -->
+<div id="fb-root"></div>
+
+<!-- Your Chat plugin code -->
+<div id="fb-customer-chat" class="fb-customerchat">
+</div>
+
+<script>
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "106582651800693");
+    chatbox.setAttribute("attribution", "biz_inbox");
+
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v12.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
 
 <script type="text/javascript" src="{{ asset('js/main.js') }}?v=4"></script>
 
